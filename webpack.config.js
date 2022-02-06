df2ca5aaf96db024758fe8e66c69808e7a5f6c75
David Wesst <david@cocobokostudios.com>
2022-02-06 06:08:29 +0000

    setup react app from scratch

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { 
+            presets: ["@babel/env", "@babel/preset-react", "@babel/preset-typescript"] 
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'public'),
+    },
+    port: 9000,
+    liveReload: false,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
