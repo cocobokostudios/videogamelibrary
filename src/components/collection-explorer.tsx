71fb19dbaf8bf48249025e7e823deed82741f46d
David Wesst <david@cocobokostudios.com>
2022-02-17 15:55:23 +0000

    added command bar to collection explorer. resolves #2

--- src/components/collection-explorer.tsx
+++ src/components/collection-explorer.tsx
@@ -1,8 +1,25 @@
 import * as React from "react";
+import { CommandBar, ICommandBarItemProps } from "@fluentui/react";
+import { registerIcons } from '@fluentui/react/lib/Styling';
+import { initializeIcons } from "@fluentui/react";
 
 import styles from "../styles/collection-explorer.module.css";
 
-export const COLLECTION_EXPLORER_TEST_ID = "collection-explorer";
+export const COLLECTION_EXPLORER_TESTID = "collection-explorer";
+export const COLLECTION_EXPLORER_COMMAND_BAR_TESTID = "collection-explorere_commandbar";
+
+const commandItems: ICommandBarItemProps[] = [
+    {
+        key: "fileCommandItem",
+        text: "File",
+        iconProps: { iconName: "DocumentManagement" }
+    },
+    {
+        key: "exploreCommandItem",
+        text: "Explore",
+        iconProps: { iconName: "ExploreData" }
+    }
+]
 
 interface CollectionExplorerProps {
     sampleMsg?: string;
@@ -18,10 +35,16 @@ class CollectionExplorer extends React.Component<CollectionExplorerProps, Collec
     };
 
     render() {
-        const displayMessage = (this.props.sampleMsg) ? this.props.sampleMsg : "Welcome to the collection explorer";
+        //const displayMessage = (this.props.sampleMsg) ? this.props.sampleMsg : "Welcome to the collection explorer";
+
+        // setup react icons
+        initializeIcons();
+
         return (
-            <section data-testid={COLLECTION_EXPLORER_TEST_ID} className={styles.CollectionExplorer} >
-                {displayMessage} | {this.state.sampleCount}
+            <section data-testid={COLLECTION_EXPLORER_TESTID} className={styles.CollectionExplorer} >
+                <header>
+                    <CommandBar data-testid={COLLECTION_EXPLORER_COMMAND_BAR_TESTID} items={commandItems} />
+                </header>
             </section>
         )
     }
--- test/collection-explorer.test.tsx
+++ test/collection-explorer.test.tsx
@@ -1,8 +1,20 @@
-import { cleanup, render, waitFor, screen, within } from "@testing-library/react";
+import { cleanup, render, waitFor, screen, within, getAllByRole } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import * as React from "react";
 
-import CollectionExplorer from "../src/components/collection-explorer";
+import CollectionExplorer, { COLLECTION_EXPLORER_COMMAND_BAR_TESTID } from "../src/components/collection-explorer";
 
-it.todo("has not be implemented yet");
+afterEach(cleanup);
+
+it("has the command bar in the header", async ()=> {
+    // arrange
+    render(<CollectionExplorer />);
+
+    // act
+    await waitFor(()=> screen.getAllByRole("banner"));
+
+    // assert
+    const commmandBarElement = screen.getByTestId(COLLECTION_EXPLORER_COMMAND_BAR_TESTID);
+    expect(commmandBarElement).toBeTruthy();
+});
