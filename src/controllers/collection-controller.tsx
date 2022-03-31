ddf1ae0d9cc22ad18dc266d730ac014939d8f4dc
David Wesst <david@cocobokostudios.com>
2022-03-31 01:45:05 +0000

    WIP adding tests

--- src/controllers/collection-controller.tsx
+++ src/controllers/collection-controller.tsx
@@ -1,10 +1,20 @@
 import Game from "../models/game";
 
 class CollectionController {
-    private static instance: CollectionController;
     
-    private constructor() { }   // singleton pattern
-    private getInstance() {
+    // singleton pattern
+    private static instance: CollectionController;
+    private constructor() { 
+        this.collection = new Array<Game>();
+    }   
+
+    public collection: Array<Game>;
+    
+    /**
+     * Gets or intializes the singleton instance of the CollectionController.
+     * @returns {CollectionController} Singleton instance of CollectionController
+     */
+    static getInstance() {
         if(!CollectionController.instance) {
             CollectionController.instance = new CollectionController();
         }
@@ -14,13 +24,18 @@ class CollectionController {
 
     /**
      * @param {string} filePath Path to the local file with game collection data
-     * @returns {void}
+     * @returns {number} Returns Number of invalid game objects read from the file.
      */
     loadCollectionFromFile(filePath: string) {
 
     }
 
-    loadCollection(collection: Array<Game>) {
+    /**
+     * Loads all valid and invalid game entries from JSON array into CollectionExplorer collection property.
+     * @param collection JSON array of {@type Game} objects.
+     * @returns {number} Returns Number of invalid game objects read from the collection.
+     */
+    loadCollectionFromJSON(collection: Array<Game>) {
 
     }
 }
--- test/controllers/collection-controller.test.tsx
+++ test/controllers/collection-controller.test.tsx
@@ -1,6 +1,37 @@
 import CollectionController from "../../src/controllers/collection-controller";
+import Game from "../../src/models/game";
 
-it.todo("cannot be initalized with 'new' (Singleton Only)");
-it.todo("loads all valid games from a file into the in-memory collection");
-it.todo("logs all invalid game from a file as invalid on load");
+import testCollection from "../data/test-collection.json";
+
+it("returns a singleton instance", ()=> {
+    // arrange & act
+    const target = CollectionController.getInstance();
+    const otherTarget = CollectionController.getInstance();
+
+    // assert
+    expect(Object.is(target, otherTarget)).toBe(true);
+});
+
+it("initializes with an empty collection", ()=> {
+    // arrange & act
+    const target = CollectionController.getInstance();
+
+    // act
+    expect(target.collection).toBeTruthy();
+    expect(target.collection.length).toEqual(0);
+});
+
+it("loads all valid games from JSON into the in-memory collection", ()=> {
+    // arrange
+    const target = CollectionController.getInstance();
+
+    // act
+    const result = target.loadCollectionFromJSON(testCollection);
+
+    // assert
+    expect(result).toEqual(0);
+    expect(target.collection.length).toEqual(testCollection.length);
+});
+
+it.todo("returns number of invalid");
 it.todo("saves a game collection from memory into local storage");
