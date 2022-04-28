08189bf1d27bd88335984e5f6b22a39a15892c86
David Wesst <david@cocobokostudios.com>
2022-04-28 16:56:30 +0000

    loading collection into explorer state. relates to #8

--- src/components/collection-explorer.tsx
+++ src/components/collection-explorer.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { CommandBar, ICommandBarItemProps, Dialog, IDialogContentProps, DialogFooter, PrimaryButton } from "@fluentui/react";
+import { CommandBar, ICommandBarItemProps, Dialog, IDialogContentProps, DialogFooter, PrimaryButton, DefaultButton } from "@fluentui/react";
 import { useBoolean } from "@fluentui/react-hooks";
 
 import styles from "../styles/collection-explorer.module.css";
 import CollectionController from "../controllers/collection-controller";
+import Game from "../models/game";
 
 export const COLLECTION_EXPLORER_TESTID = "collection-explorer";
 export const COLLECTION_EXPLORER_COMMAND_BAR_TESTID = "collection-explorer_commandbar";
@@ -11,32 +12,57 @@ export const COLLECTION_EXPLORER_LOAD_COLLECTION_DIALOG_TESTID = "collection-exp
 
 const CollectionExplorer : React.FunctionComponent = () =>  {
     const [hideLoadDialog, { toggle: toggleHideLoadDialog }] = useBoolean(true);
+    const [loadedCollection, setLoadedCollection] = React.useState<Array<Game>>([]);
+    const [collection, setCollection] = React.useState<Array<Game>>([]);
+
     const loadDialogContentProps: IDialogContentProps = {
         title: "Load Collection",
         showCloseButton: true,
         closeButtonAriaLabel: "Close"
     };
-    const collectionController = CollectionController.getInstance();
-
     const commandItems: ICommandBarItemProps[] = [
         {
             key: "loadCommandItem",
             text: "Load",
             iconProps: { iconName: "Upload" },
-            onClick: toggleHideLoadDialog
+            onClick: toggleHideLoadDialog 
         }
     ];
 
-    const onFileInputChange = async (e:React.SyntheticEvent) => {
+
+
+    const onFileInputChange = async (e: React.SyntheticEvent) => {
         e.preventDefault();
 
         // check for files
         const fileInput: HTMLInputElement = e.currentTarget as HTMLInputElement;
         if(fileInput && fileInput.files) {
             const files: FileList = fileInput.files;
-            const loadedCollection = await CollectionController.getInstance().loadCollectionFromFile(files[0]);
-            console.dir(loadedCollection);
+            setLoadedCollection(await CollectionController.getInstance().loadCollectionFromFile(files[0]));
         } 
+    }
+
+    const loadButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        // update the collection
+        setCollection(loadedCollection);
+
+        // clear loaded collection
+        setLoadedCollection([]);
+
+        // close dialogue
+        toggleHideLoadDialog();
+    }
+
+    const cancelButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        // clear loaded collection
+        setLoadedCollection([]);
+
+        // close dialogue
+        toggleHideLoadDialog();
     }
 
     return (
@@ -46,6 +72,8 @@ const CollectionExplorer : React.FunctionComponent = () =>  {
                 <CommandBar data-testid={COLLECTION_EXPLORER_COMMAND_BAR_TESTID} items={commandItems} />
             </header>
             <main>
+                <p>There are {collection.length} games in the collection.</p>
+                <p>There are {loadedCollection.length} games in the loaded collection.</p>
             </main>
         </section>
         <Dialog
@@ -58,7 +86,8 @@ const CollectionExplorer : React.FunctionComponent = () =>  {
                     accept="text/csv"
                     onChange={onFileInputChange} />
             <DialogFooter>
-                <PrimaryButton onClick={toggleHideLoadDialog} text="Load" />
+                <PrimaryButton onClick={loadButtonClick} text="Load" />
+                <DefaultButton onClick={cancelButtonClick} text="Cancel" />
             </DialogFooter>
         </Dialog>
         </>
