7ceecd331fe712eef2005bdc4be06a90fb2a53c6
David Wesst <david@cocobokostudios.com>
2022-05-10 04:31:44 +0000

    minimized game model and added serialize function

---
+++ src/models/game.ts
@@ -0,0 +1,63 @@
+import Ajv, { KeywordDefinition } from "ajv";
+
+import ILogger from "../utils/ILogger";
+import ConsoleLogger from "../utils/ConsoleLogger";
+import * as GameSchema from "../schemas/game.schema.json";
+import { isValidElement } from "react";
+
+/**
+ * Creates a Game object that represents a game in a collection.
+ * @class
+ * 
+ */
+class Game {
+    /** @member {string} gameId Unique identifier for the game */
+    public gameId: string;
+    /** @member {string} title The title of the game that is displayed in user interfaces */
+    public title: string;
+    /** @member {string} platformId Unique identifier that references the platform the game runs on  */
+    public platformId: string;
+    /** @member {string} regionId Unique identifier that references the region the game is for  */
+    public regionId: string;
+    /** @member {string} price The record CAD value of the game. Defaults to Number.NaN if left unset.  */
+    public price: number;
+    
+    private logger: ILogger = ConsoleLogger.getInstance();
+
+    constructor(gameId: string, gameTitle: string, platformId: string, regionId: string, price: number = Number.NaN) {        
+        this.gameId = gameId;
+        this.title = gameTitle;
+        this.platformId = platformId;
+        this.regionId = regionId;
+        this.price = price;
+    }
+
+
+    /**
+     * Determine if the Game object has all required fields.
+     * @returns {boolean} True, if all required fields have valid values.
+     */
+    public hasRequiredFields() {
+        let isValid: boolean = false;
+
+        isValid = (this.gameId !== undefined && this.gameId.trim().length > 0) 
+                    && (this.title !== undefined && this.title.trim().length > 0)
+                    && (this.platformId !== undefined && this.platformId.trim().length > 0)
+                    && (this.regionId !== undefined && this.regionId.trim().length > 0)
+        
+        return isValid;
+    }
+
+    public serialize() : string {
+        const gameData = {
+            gameId: this.gameId,
+            title: this.title,
+            platformId: this.platformId,
+            regionId: this.regionId,
+            price: (Number.isNaN(this.price)) ? Number.NaN.toString() : this.price
+        }
+        return JSON.stringify(gameData);
+    }
+
+}
+export default Game;
--- test/models/game.test.tsx
+++ test/models/game.test.tsx
@@ -1,53 +1,37 @@
-import { mock } from "jest-mock-extended";
-
 import Game from "../../src/models/game";
-import ILogger from "../../src/utils/ILogger";
 
-beforeEach(()=> {
-    Game.logger = mock<ILogger>();
-});
-
-it("can validate JSON string against the JSON schema", ()=> {
+it("serializes all properties to JSON", ()=> {
     // arrange
-    const validGameData = {
-        gameId: "sample-game",
-        title: "Sample Game",
-        platform: "Sample Platform"
-    }
-    const missingGameId = {
-        title: "Sample Game",
-        platform: "Sample Platform"
-    }
-    const missingTitle = {
-        gameId: "sample-game",
-        platform: "Sample Platform"
-    }
-    const missingPlatform = {
-        gameId: "sample-game",
-        title: "Sample Game",
-    }
+    const testId = "testId";
+    const testTitle = "testTitle";
+    const testPlatform = "testPlatform";
+    const testRegion = "testRegion";
+    const testPrice = 1.23;
+    const targetNoPrice = new Game(testId, testTitle, testPlatform, testRegion);
+    const targetWithPrice = new Game(testId, testTitle, testPlatform, testRegion, testPrice);
 
     // act
-    const validResult = Game.validateJSON(JSON.stringify(validGameData));
-    const missingGameIdResult = Game.validateJSON(JSON.stringify(missingGameId));
-    const missingTitleResult = Game.validateJSON(JSON.stringify(missingTitle));
-    const missingPlatformResult = Game.validateJSON(JSON.stringify(missingPlatform));
+    const resultNoPrice = targetNoPrice.serialize();
+    const resultWithPrice = targetWithPrice.serialize();
 
     // assert
-    expect(validResult).toEqual(true);
-    expect(missingGameIdResult).toEqual(false);
-    expect(missingTitleResult).toEqual(false);
-    expect(missingPlatformResult).toEqual(false);
-});
+    const expectedNoPriceObject =
+    {
+        gameId: testId,
+        title: testTitle,
+        platformId: testPlatform,
+        regionId: testRegion,
+        price: Number.NaN.toString()
+    };
+    expect(resultNoPrice).toBe(JSON.stringify(expectedNoPriceObject));
 
-it("returns false on invalid JSON string", ()=> {
-    // arrange
-    const invalidJSON = "invalid json is this!";
-
-    // act 
-    const result = Game.validateJSON(invalidJSON);
-
-    // assert
-    expect(result).toBe(false);
-    expect(Game.logger.warn).toBeCalled();
+    const expectedWithPriceObject =
+    {
+        gameId: testId,
+        title: testTitle,
+        platformId: testPlatform,
+        regionId: testRegion,
+        price: testPrice
+    };
+    expect(resultWithPrice).toBe(JSON.stringify(expectedWithPriceObject));
 });
